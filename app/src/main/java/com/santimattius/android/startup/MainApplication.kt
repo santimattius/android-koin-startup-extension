431102890139594364d62e92f6c603e96a820213
Santiago Mattiauda <santimattius@gmail.com>
2024-09-30 20:17:41 -0300

    [feature] single initializer

--- app/src/main/java/com/santimattius/android/startup/MainApplication.kt
+++ app/src/main/java/com/santimattius/android/startup/MainApplication.kt
@@ -2,27 +2,29 @@ package com.santimattius.android.startup
 
 import android.app.Application
 import android.util.Log
-import com.santimattius.android.koin.startup.KoinDefinition
-import com.santimattius.android.koin.startup.KoinFeatureExperimental
-import com.santimattius.android.koin.startup.ModuleScope
-import com.santimattius.android.startup.service.CrashTrackerService
+import com.santimattius.android.koin.startup.KoinDeclaration
 import com.santimattius.android.startup.service.AppService
-import org.koin.core.module.Module
+import com.santimattius.android.startup.service.CrashTrackerService
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.logger.Level
+import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
 
-class MainApplication : Application(), KoinDefinition {
+class MainApplication : Application(), KoinDeclaration {
 
     override fun onCreate() {
         super.onCreate()
         Log.i(this::class.simpleName, "onCreate: application created")
     }
 
-    override fun modules(): List<Module> {
-        return listOf(appModule)
+    override fun declaration(): KoinAppDeclaration {
+        return {
+            androidLogger(Level.DEBUG)
+            androidContext(this@MainApplication)
+            modules(appModule)
+        }
     }
-
-    @OptIn(KoinFeatureExperimental::class)
-    override fun scope() = ModuleScope.APP
 }
 
 val appModule = module {
