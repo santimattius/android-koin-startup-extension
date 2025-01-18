c9e307f5651ed002fc32c12fdd86d2cde9c5c1c8
Santiago Mattiauda <santimattius@gmail.com>
2025-01-18 14:14:01 -0300

    [feature] improvement code and add kDoc

--- feature/src/main/java/com/santimattius/android/feature/FeatureInitializer.kt
+++ feature/src/main/java/com/santimattius/android/feature/FeatureInitializer.kt
@@ -1,11 +1,9 @@
 package com.santimattius.android.feature
 
 import android.content.Context
-import androidx.startup.Initializer
 import com.santimattius.android.koin.startup.KoinStartupExtensionInitializer
-import com.santimattius.android.koin.startup.loadLazyModules
 
-class FeatureInitializer : Initializer<Unit> by KoinStartupExtensionInitializer() {
+class FeatureInitializer : KoinStartupExtensionInitializer<Unit>() {
 
     override fun create(context: Context) {
         loadLazyModules(featureModule)
--- koin-startup/src/main/java/com/santimattius/android/koin/startup/KoinStartupExtension.kt
+++ koin-startup/src/main/java/com/santimattius/android/koin/startup/KoinStartupExtension.kt
@@ -3,5 +3,9 @@ package com.santimattius.android.koin.startup
 import org.koin.androix.startup.KoinStartup
 import org.koin.core.annotation.KoinExperimentalAPI
 
+
+/**
+ * KoinStartupExtension is org.koin.androix.startup.KoinStartup alias
+ */
 @KoinExperimentalAPI
 typealias KoinStartupExtension = KoinStartup
