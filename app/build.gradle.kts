eb41d034df8c7fd50c5624771bb32d0c9f627040
Santiago Mattiauda <santimattius@gmail.com>
2025-03-22 12:55:45 -0300

    Benchmark (#5)
    * [feature] fix local dependency
    
    * [feature] remove module
    
    * [release] 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.santimattius.android.startup"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
